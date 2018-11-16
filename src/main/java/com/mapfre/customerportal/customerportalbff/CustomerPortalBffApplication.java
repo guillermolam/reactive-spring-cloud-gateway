3bd2f785e040e5ca752ae575b19e3acac583c192
Guillermo Lam <glam@mapfreusa.com>
2018-11-16 13:04:24 -0500

    Initial commit

---
+++ src/main/java/com/mapfre/customerportal/customerportalbff/CustomerPortalBffApplication.java
@@ -0,0 +1,12 @@
+package com.mapfre.customerportal.customerportalbff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerPortalBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerPortalBffApplication.class, args);
+	}
+}
