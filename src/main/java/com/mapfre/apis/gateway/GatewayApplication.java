ce19e3626c270ca07a956b2461daba2a56773b33
adatla <adatla@mapfreusa.com>
2019-01-15 17:40:24 -0500

    initial commit

---
+++ src/main/java/com/mapfre/apis/gateway/GatewayApplication.java
@@ -0,0 +1,23 @@
+package com.mapfre.apis.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) throws Exception {
+		return http.httpBasic().and().csrf().disable().authorizeExchange().anyExchange().permitAll().and().build();
+	}
+
+}
