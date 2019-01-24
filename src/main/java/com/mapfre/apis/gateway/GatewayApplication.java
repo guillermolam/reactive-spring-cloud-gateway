869aa40394eada0a40eea4a288c3222af311d615
adatla <adatla@mapfreusa.com>
2019-01-24 11:12:51 -0500

    Making dependency changes

--- src/main/java/com/mapfre/apis/gateway/GatewayApplication.java
+++ src/main/java/com/mapfre/apis/gateway/GatewayApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableWebFluxSecurity
 public class GatewayApplication {
 
 	public static void main(String[] args) {
