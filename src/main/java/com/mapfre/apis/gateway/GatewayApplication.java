2785625e8278b3e65dd600e07e6fe0f3d424af56
adatla <adatla@mapfreusa.com>
2019-01-31 13:29:08 -0500

    fixing password grant type

--- src/main/java/com/mapfre/apis/gateway/GatewayApplication.java
+++ src/main/java/com/mapfre/apis/gateway/GatewayApplication.java
@@ -1,27 +1,27 @@
 package com.mapfre.apis.gateway;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.security.oauth2.gateway.TokenRelayGatewayFilterFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @SpringBootApplication
-//@EnableDiscoveryClient
 public class GatewayApplication {
 
 	@Autowired
 	private TokenRelayGatewayFilterFactory filterFactory;
+
+	@Value("${client-id}")
+	private String clientId;
+
+	@Value("${client-secret}")
+	private String clientsecret;
 
 	@Bean
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
@@ -53,16 +53,14 @@ public class GatewayApplication {
 				.route("billing", r -> r.path("/billing-api/**").filters(
 						f -> f.rewritePath("/billing-api/(?<segment>.*)", "/${segment}").filter(filterFactory.apply()))
 						.uri("https://billing-api.apps.nonprod.us-east-1.aws.pcf.mapfreusa.com"))
+				// Authorization and Token Endpoint
+				.route("auth",
+						r -> r.path("/auth/**")
+								.filters(f -> f.rewritePath("/auth/(?<segment>.*)", "/${segment}")
+										.filter(filterFactory.apply()).addRequestParameter("client_id", clientId)
+										.addRequestParameter("client_secret", clientsecret))
+								.uri("https://external-dev.login.sys.nonprod.us-east-1.aws.pcf.mapfreusa.com"))
 				.build();
-	}
-
-	@GetMapping("/")
-	public String index(Model model, @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
-			@AuthenticationPrincipal OAuth2User oauth2User) {
-		model.addAttribute("userName", oauth2User.getName());
-		model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
-		model.addAttribute("userAttributes", oauth2User.getAttributes());
-		return "index";
 	}
 
 	public static void main(String[] args) {
--- src/main/java/com/mapfre/apis/gateway/security/config/SecurityConfig.java
+++ src/main/java/com/mapfre/apis/gateway/security/config/SecurityConfig.java
@@ -1,14 +1,20 @@
 package com.mapfre.apis.gateway.security.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
-//@Configuration
+@Configuration
 public class SecurityConfig {
 
-	//@Bean
+	@Bean
 	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-		http.authorizeExchange().anyExchange().permitAll().and().csrf().disable();
+//		http.csrf().disable().authorizeExchange().pathMatchers("/auth/**").permitAll();
+//		return http.build();
+		http.csrf().disable().cors().disable()
+		.authorizeExchange().pathMatchers("/actuator/**").permitAll().and()
+		.authorizeExchange().pathMatchers("/auth/**").permitAll();
 		return http.build();
 	}
 
